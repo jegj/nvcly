beabc5db736793d6d92adaffef0a3b9a170b29f1
Javier Galarza <jegj57@gmail.com>
2025-01-12 07:41:19 -0500

    chore: sample using list

--- main.go
+++ main.go
@@ -2,92 +2,66 @@ package main
 
 import (
 	"log"
-	"math"
-	"time"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+	"github.com/jegj/nvcly/src/nvidiasmi"
 )
 
 func main() {
+	nvidiasmi.GetNvidiaSmiStats()
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 
-	sinFloat64 := (func() []float64 {
-		n := 400
-		data := make([]float64, n)
-		for i := range data {
-			data[i] = 1 + math.Sin(float64(i)/5)
-		}
-		return data
-	})()
-
-	sl := widgets.NewSparkline()
-	sl.Data = sinFloat64[:100]
-	sl.LineColor = ui.ColorCyan
-	sl.TitleStyle.Fg = ui.ColorWhite
-
-	slg := widgets.NewSparklineGroup(sl)
-	slg.Title = "Sparkline"
-
-	lc := widgets.NewPlot()
-	lc.Title = "braille-mode Line Chart"
-	lc.Data = append(lc.Data, sinFloat64)
-	lc.AxesColor = ui.ColorWhite
-	lc.LineColors[0] = ui.ColorYellow
-
-	gs := make([]*widgets.Gauge, 3)
-	for i := range gs {
-		gs[i] = widgets.NewGauge()
-		gs[i].Percent = i * 10
-		gs[i].BarColor = ui.ColorRed
-	}
-
-	ls := widgets.NewList()
-	ls.Rows = []string{
-		"[1] Downloading File 1",
-		"",
-		"",
-		"",
-		"[2] Downloading File 2",
-		"",
-		"",
-		"",
-		"[3] Uploading File 3",
-	}
-	ls.Border = false
-
-	p := widgets.NewParagraph()
-	p.Text = "<> This row has 3 columns\n<- Widgets can be stacked up like left side\n<- Stacked widgets are treated as a single widget"
-	p.Title = "Demonstration"
-
 	grid := ui.NewGrid()
 	termWidth, termHeight := ui.TerminalDimensions()
 	grid.SetRect(0, 0, termWidth, termHeight)
 
+	main := widgets.NewList()
+	main.Title = "Attached GPUs"
+	main.Rows = []string{
+		"[0] github.com/gizak/termui/v3",
+		"[1] [你好，世界](fg:blue)",
+		"[2] [こんにちは世界](fg:red)",
+		"[3] [color](fg:white,bg:green) output",
+		"[4] output.go",
+		"[5] random_out.go",
+		"[6] dashboard.go",
+		"[7] foo",
+		"[8] bar",
+		"[9] baz",
+	}
+	main.TextStyle = ui.NewStyle(ui.ColorYellow)
+	main.WrapText = false
+
+	sidebar := widgets.NewList()
+	sidebar.Title = "General Info"
+	sidebar.Rows = []string{
+		"[0] github.com/gizak/termui/v3",
+		"[1] [你好，世界](fg:blue)",
+		"[2] [こんにちは世界](fg:red)",
+		"[3] [color](fg:white,bg:green) output",
+		"[4] output.go",
+		"[5] random_out.go",
+		"[6] dashboard.go",
+		"[7] foo",
+		"[8] bar",
+		"[9] baz",
+	}
+	sidebar.TextStyle = ui.NewStyle(ui.ColorYellow)
+	sidebar.WrapText = false
+
 	grid.Set(
-		ui.NewRow(1.0/2,
-			ui.NewCol(1.0/2, slg),
-			ui.NewCol(1.0/2, lc),
-		),
-		ui.NewRow(1.0/2,
-			ui.NewCol(1.0/4, ls),
-			ui.NewCol(1.0/4,
-				ui.NewRow(.9/3, gs[0]),
-				ui.NewRow(.9/3, gs[1]),
-				ui.NewRow(1.2/3, gs[2]),
-			),
-			ui.NewCol(1.0/2, p),
+		ui.NewRow(1,
+			ui.NewCol(3.0/4, main),
+			ui.NewCol(1.0/4, sidebar),
 		),
 	)
 
 	ui.Render(grid)
-
-	tickerCount := 1
 	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Second).C
 	for {
 		select {
 		case e := <-uiEvents:
@@ -100,17 +74,6 @@ func main() {
 				ui.Clear()
 				ui.Render(grid)
 			}
-		case <-ticker:
-			if tickerCount == 100 {
-				return
-			}
-			for _, g := range gs {
-				g.Percent = (g.Percent + 3) % 100
-			}
-			slg.Sparklines[0].Data = sinFloat64[tickerCount : tickerCount+100]
-			lc.Data[0] = sinFloat64[2*tickerCount:]
-			ui.Render(grid)
-			tickerCount++
 		}
 	}
 }
